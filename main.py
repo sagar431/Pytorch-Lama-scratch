f27eb381aa380c837ddfa44e244da3a831848e9f
Sagar pallai <36654027+sagar431@users.noreply.github.com>
2024-01-11 09:23:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import torch
+from inference import LLaMA
+
+if __name__ == '__main__':
+    torch.manual_seed(1)
+    use_cuda = True
+    device = "cuda" if torch.cuda.is_available() and use_cuda else "cpu"
+    prompts = [
+        "Simply put, the theory of relativity states that ",
+        "If Google was an Italian company founded in Milan, it would",
+        # Few shot promt
+        """Translate English to French:
+
+        sea otter => loutre de mer
+        peppermint => menthe poivrée
+        plush girafe => girafe peluche
+        cheese => ?""",
+        "Tell me the rules of cricket"
+    ]
+
+    model = LLaMA.build(
+        checkpoints_dir='llama-2-7b/',
+        tokenizer_path='tokenizer.model',
+        load_model=True,
+        max_seq_len=1024,
+        max_batch_size=len(prompts),
+        device=device
+    )
+
+    out_tokens, out_texts = (model.text_completion(prompts, max_gen_len=64, device=device))
+    assert len(out_texts) == len(prompts)
+    for i in range(len(out_texts)):
+        print(f'{out_texts[i]}')
+        print('-' * 50)
+
+
